b1bd55cebe22268a87eef0b6cfc59250f3249ace
Xun Li <xunli@uchicago.edu>
2020-03-19 16:15:36 -0700

    update county merge logic #2

--- tools/hourly_update.py
+++ tools/hourly_update.py
@@ -42,12 +42,14 @@ def read_covid_data(cr):
         case_id, confirmed_date,state_name,county_name,confirmed_count,death_count = row
         confirmed_count = (int)(confirmed_count)
         death_count = (int)(death_count)
+
         if state_name not in state_count:
             state_count[state_name] = 0
             state_deathcount[state_name] = 0
         state_count[state_name] += confirmed_count
         state_deathcount[state_name] += death_count
 
+        county_name = county_name + state_name
         if county_name not in county_count:
             county_count[county_name] = 0
             county_deathcount[county_name] = 0
@@ -107,11 +109,11 @@ def update_state_geojson(state_count, state_deathcount, date_state_count, date_s
             json.dump(geojson, outfile)
 
 def update_county_geojson(county_count, county_deathcount, date_county_count, date_county_deathcount):
-    with open("counties.geojson") as f:
+    with open("../data/county_2018.geojson") as f:
         geojson = json.load(f)
         features = geojson["features"]
         for feat in features:
-            county_id = feat["properties"]["NAME"]
+            county_id = feat["properties"]["NAME"] + feat["properties"]["state_abbr"]
 
             if county_id in county_count:
                 feat["properties"]["confirmed_count"] = county_count[county_id]
@@ -135,7 +137,10 @@ def update_county_geojson(county_count, county_deathcount, date_county_count, da
         with open('counties_update.geojson', 'w') as outfile:
             json.dump(geojson, outfile)
 
-fetch_covid_data()
+#fetch_covid_data()
+with open("cases.csv") as csvfile:
+    cr = csv.reader(csvfile)
+    read_covid_data(cr)
 
 #scheduler = BlockingScheduler()
 #scheduler.add_job(fetch_covid_data, 'interval', hours=1)
