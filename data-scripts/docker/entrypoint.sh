94fc5dfa15fc136db262751553fea7870af62e99
vidal-anguiano <vidal.anguiano@gmail.com>
2020-10-07 23:28:02 -0500

    updates to docker scripts

--- data-scripts/docker/entrypoint.sh
+++ data-scripts/docker/entrypoint.sh
@@ -1,30 +1,52 @@
 
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-git config --global user.email "spatial@uchicago.edu"
-git config --global user.name "COVID Data Bot"
+git config --global user.email "theuscovidatlas@gmail.com"
+git config --global user.name "theuscovidatlas"
 
-git clone git@github.com:GeoDaCenter/covid.git && cd covid
-git checkout master
+
+case $REPO in
+	covid)
+		git clone git@github.com:GeoDaCenter/covid.git && cd covid
+	;;
+
+	testing)
+		git clone git@github.com:GeoDaCenter/covid-atlas-research.git && cd covid-atlas-research/Testing_Data/python
+
+	;;
+esac
 
 case $DATA_SOURCE in
 
 	berkeley_predictions)
-		export COMMAND="python data-scripts/berkeley_predictions/berkeley_predictions.py"
+		export COMMAND="python ./data-scripts/berkeley_predictions/berkeley_predictions.py"
 		;;
 
 	usafacts)
-	  export COMMAND="python data-scripts/usafacts/usafacts.py"
+	  export COMMAND="python ./data-scripts/usafacts/usafacts.py"
 		;;
 
 	1p3a)
-		export COMMAND="python data-scripts/_1p3a/_1p3a.py"
+		export COMMAND="python ./data-scripts/_1p3a/_1p3a.py"
 		;;
+
+	lisa)
+		export COMMAND="python ./data-scripts/lisa/update_lisa_json.py"
+		;;
+
+	testing)
+		export COMMAND='bash scrape.py'
 
 esac
 
+
 if $COMMAND;
-		then git add . && git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"&& git push;
+ 		then
+		elif [ "$DATA_SOURCE" = "lisa" ];
+	  	then exit 0;
+		else
+			then git add . && git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"&& git push;
+		fi
 else
-	echo "$DATA_SOURCE script failed."
+	echo "$DATA_SOURCE script failed.";
 	exit 1;
 fi
