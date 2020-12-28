983c986bf15661326a412001cb655b078b26269b
Dylan <d_h@mit.edu>
2020-12-28 14:24:47 -0600

    CDC data update

--- data-scripts/cdc/getCdcData.py
+++ data-scripts/cdc/getCdcData.py
@@ -1,100 +1,125 @@
 import pandas as pd
 from datetime import datetime, date, timedelta
-import urllib.request
+from bs4 import BeautifulSoup
+from glob import glob
+import json, requests
 
-def getCdcData():
-    # get today's date
-    today = date.today() - timedelta(days=1)
+def downloadCDCdata():
+    # front end CDC page with links
+    URL = 'https://beta.healthdata.gov/National/COVID-19-Community-Profile-Report/gqxm-d9w9'
 
-    # read in current CDC excel
-    # sheet name is "Counties"
-    url = f"https://beta.healthdata.gov/api/views/gqxm-d9w9/files/df6ad32a-2fe8-4878-ae3d-a9d972c85ea8?download=true&filename=Community_Profile_Report%{today.year}{today.month}{today.day}_Public.xlsx"
-    raw = pd.read_excel(url, sheet_name="Counties")
-    urllib.request.urlretrieve(url, f"./xlsx/Community_Profile_Report_{today.year}{today.month}{today.day}_Public.xlsx")
+    # download Page as HTML
+    page = requests.get(URL)
 
-    # parse date ranges from Excel
-    endOfWeekCases = f"{raw.columns[14].split('(')[-1].split(' ')[0]} {raw.columns[14].split('-')[-1][:-1]}"
-    endofWeekCases = datetime.strptime(endOfWeekCases, '%B %d')
+    # parse HTML with BS4
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-    endOfWeekTesting = f"{raw.columns[32].split('(')[-1].split(' ')[0]} {raw.columns[32].split('-')[-1][:-1]}"
-    endOfWeekTesting = datetime.strptime(endOfWeekTesting, '%B %d')
+    # find the script block with the URLs we need
+    for a in soup.find_all('script'):
+        if 'initialState' in str(a):
+            linkBlock = str(a)[69:-17].split('},{')
 
-    endOfWeekForecasting = f"{raw.columns[65].split('(')[-1].split(' ')[0]} {raw.columns[65].split(' ')[-1][:-1]}"
-    endOfWeekForecasting = datetime.strptime(endOfWeekForecasting, '%B %d')
+    # declare list to be populated
+    cleanedLinks = []
 
-    # Read in past data
-    prevCases = pd.read_csv('./csv/covid_confirmed_cdc.csv')
-    prevDeaths = pd.read_csv('./csv/covid_deaths_cdc.csv')
-    prevTesting = pd.read_csv('./csv/covid_testing_cdc.csv')
-    prevTestingPos = pd.read_csv('./csv/covid_wk_pos_cdc.csv')
-    prevTestingCap = pd.read_csv('./csv/covid_tcap_cdc.csv')
-    prevTestingCcpt = pd.read_csv('./csv/covid_ccpt_cdc.csv')
+    # pull out clean links to list
+    # the reason for this is that the links are not just dates --
+    # each has a UUID we need to scrape in order to download the excel
+    for link in linkBlock:
+        cleaned = link.split('"href":"')[-1].split(',"link"')[0]
+        if ('Community_Profile_Report' in cleaned) and ('/api/views/' in cleaned) and ('Public.xlsx' in cleaned) and (len(cleaned) < 300):
+            cleanedLinks.append(f"https://beta.healthdata.gov{cleaned}")
 
-    # Assign column names 
-    # Raw Excel is styled with names in row 2
-    raw.columns = list(raw.iloc[0])
-    raw = raw.drop(index=0)
+    # check out current excel files
+    downloadFiles = glob('./xlsx/*.xlsx')
 
-    # Pull in relevant data and name columns based on date
-    cases = raw[['FIPS code', 'Cumulative cases']]
-    cases.columns = ['FIPS', f"{endofWeekCases.month}/{endofWeekCases.day}/{today.year}"]
+    # download any files we're missing in the xlsx folder
+    for url in cleanedLinks:
+        r = requests.get(url)
+        date = url[-21:-13]
+        if f"./xlsx\\CDC_{url[-21:-13]}.xlsx" not in downloadFiles:
+            with open(f"./xlsx/CDC_{date}.xlsx", 'wb') as f:
+                f.write(r.content)
 
-    deaths = raw[['FIPS code', 'Cumulative deaths']]
-    deaths.columns = ['FIPS', f"{endofWeekCases.month}/{endofWeekCases.day}/{today.year}"]
+def findColumn(columns, search):
+    for column in columns:
+        if search.lower() in column.lower():
+            return column
+    return None
 
-    testingPos = raw[['FIPS code',  'Viral (RT-PCR) lab test positivity rate - last 7 days (may be an underestimate due to delayed reporting)']]
-    testingPos.columns = ['FIPS', f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}"]
+def parseCDCdata(fileList):
+    for index, file in enumerate(fileList):
+        # import excel sheets
+        raw = pd.read_excel(file, sheet_name="Counties")
+        # snag case and testing dates -- these are different each day 
+        # typically the testing weekly data is 2 days behind  ¯\_(ツ)_/¯
+        endOfWeekCases = f"{raw.columns[14].split('(')[-1].split(' ')[0]} {raw.columns[14].split('-')[-1][:-1]}"
+        endofWeekCases = datetime.strptime(endOfWeekCases, '%B %d')
+        endOfWeekTesting = f"{raw.columns[32].split('(')[-1].split(' ')[0]} {raw.columns[32].split('-')[-1][:-1]}"
+        endOfWeekTesting = datetime.strptime(endOfWeekTesting, '%B %d')
+        year = file[-13:-9]
+        # set second row as column index
+        # this is to handle the CDC excel formatting
+        raw.columns = list(raw.iloc[0])
+        raw = raw.drop(index=0)
+        columns = list(raw.columns)
 
-    testing = raw[['FIPS code', 'Total RT-PCR diagnostic tests - last 7 days (may be an underestimate due to delayed reporting)']]
-    testing.columns = ['FIPS', f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}"]
-
-    testingCap = raw[['FIPS code', 'RT-PCR tests per 100k - previous 7 days(may be an underestimate due to delayed reporting)']]
-    testingCap.columns = ['FIPS', f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}"]
-
-    # Calculate Ccpt
-    testingCcpt = raw[['FIPS code', 'Cases - last 7 days', 'Total RT-PCR diagnostic tests - last 7 days (may be an underestimate due to delayed reporting)']]
-    testingCcpt.columns = ['FIPS', 'Cases', 'Tests']
-    testingCcptFiltered = testingCcpt.query('Tests > 0')
-    testingCcptFiltered['CCPT'] = testingCcptFiltered['Cases']/testingCcptFiltered['Tests']
-    testingCcpt = testingCcpt.merge(testingCcptFiltered, on=['FIPS','Cases','Tests'], how="left")[['FIPS','CCPT']]
-    testingCcpt.columns = ['FIPS', f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}"]
-
-
-    # Test for column duplicates, drop if duplicated
-    if f"{endofWeekCases.month}/{endofWeekCases.day}/{today.year}" in prevCases.columns: 
-        prevCases = prevCases.drop(columns=[f"{endofWeekCases.month}/{endofWeekCases.day}/{today.year}"])
-
-    if f"{endofWeekCases.month}/{endofWeekCases.day}/{today.year}" in prevDeaths.columns: 
-        prevDeaths = prevDeaths.drop(columns=[f"{endofWeekCases.month}/{endofWeekCases.day}/{today.year}"])
-
-    if f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}" in prevTesting.columns: 
-        prevTesting = prevTesting.drop(columns=[f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}"])
+        # grab and format the data we want into separate DFs
+        cases = raw[['FIPS code', findColumn(list(raw.columns), 'cumulative cases')]]
+        cases.columns = ['FIPS', f"{endofWeekCases.month}/{endofWeekCases.day}/{year}"]   
         
-    if f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}" in prevTestingPos.columns: 
-        prevTestingPos = prevTestingPos.drop(columns=[f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}"])
-        
-    if f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}" in testingCap.columns: 
-        testingCap = testingCap.drop(columns=[f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}"])
-        
-    if f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}" in testingCcpt.columns: 
-        testingCcpt = testingCcpt.drop(columns=[f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{today.year}"])
+        deaths = raw[['FIPS code',findColumn(list(raw.columns), 'cumulative deaths')]]
+        cases.columns = ['FIPS', f"{endofWeekCases.month}/{endofWeekCases.day}/{year}"]
 
+        testingPos = raw[['FIPS code',  findColumn(list(raw.columns), 'test positivity rate - last 7 day')]]
+        testingPos.columns = ['FIPS', f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{year}"]
 
-    # Merge historic data with current
-    cases = prevCases.merge(cases, on="FIPS", how="outer")
-    deaths = prevDeaths.merge(deaths, on="FIPS", how="outer")
-    testing = prevTesting.merge(testing, on="FIPS", how="outer")
-    testingPos = prevTestingPos.merge(testingPos, on="FIPS", how="outer")
-    testingCap = prevTestingCap.merge(testingCap, on="FIPS", how="outer")
-    testingCcpt = prevTestingCcpt.merge(testingCcpt, on="FIPS", how="outer")
+        testing = raw[['FIPS code', findColumn(list(raw.columns), 'Total RT-PCR diagnostic tests - last 7 days')]]
+        testing.columns = ['FIPS', f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{year}"]
 
-    # export to CSV
-    cases.to_csv('./csv/covid_confirmed_cdc.csv',index=False)
-    deaths.to_csv('./csv/covid_deaths_cdc.csv',index=False)
-    testingPos.to_csv('./csv/covid_wk_pos_cdc.csv',index=False)
-    testing.to_csv('./csv/covid_testing_cdc.csv',index=False)
-    testingCap.to_csv('./csv/covid_tcap_cdc.csv',index=False)
-    testingCcpt.to_csv('./csv/covid_ccpt_cdc.csv',index=False)
+        testingCap = raw[['FIPS code', findColumn(list(raw.columns), 'RT-PCR tests per 100k - previous 7 days')]]
+        testingCap.columns = ['FIPS', f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{year}"]
+
+        testingCcpt = raw[['FIPS code',  findColumn(list(raw.columns), 'cases - last 7 days'),  findColumn(list(raw.columns), 'Total RT-PCR diagnostic tests - last 7 days')]]
+        testingCcpt.columns = ['FIPS', 'Cases', 'Tests']
+
+        # filter and calculate CCPT
+        testingCcptFiltered = testingCcpt.query('Tests > 0')
+        testingCcptFiltered['CCPT'] = testingCcptFiltered['Cases']/testingCcptFiltered['Tests']
+
+        testingCcpt = testingCcpt.merge(testingCcptFiltered, on=['FIPS','Cases','Tests'], how="left")[['FIPS','CCPT']]
+        testingCcpt.columns = ['FIPS', f"{endOfWeekTesting.month}/{endOfWeekTesting.day}/{year}"]
+
+        # join data on FIPS, check for duplicated columns
+        if index == 0:
+            prevCases = cases.copy()
+            prevDeaths = deaths.copy()
+            prevTesting = testing.copy()
+            prevTestingPos = testingPos.copy()
+            prevTestingCap = testingCap.copy()
+            prevTestingCcpt = testingCcpt.copy()
+        else:
+            if list(cases.columns)[1] not in list(prevCases.columns):
+                prevCases = prevCases.merge(cases, on="FIPS", how="outer")
+            if list(deaths.columns)[1] not in list(prevDeaths.columns):
+                prevDeaths = prevDeaths.merge(deaths, on="FIPS", how="outer")
+            if list(testing.columns)[1] not in list(prevTesting.columns):
+                prevTesting = prevTesting.merge(testing, on="FIPS", how="outer")
+            if list(testingPos.columns)[1] not in list(prevTestingPos.columns):
+                prevTestingPos = prevTestingPos.merge(testingPos, on="FIPS", how="outer")
+            if list(testingCap.columns)[1] not in list(prevTestingCap.columns):
+                prevTestingCap = prevTestingCap.merge(testingCap, on="FIPS", how="outer")
+            if list(testingCcpt.columns)[1] not in list(prevTestingCcpt.columns):
+                prevTestingCcpt = prevTestingCcpt.merge(testingCcpt, on="FIPS", how="outer")
+    
+    # after loop, export to CSV
+    prevCases.to_csv('./csv/covid_confirmed_cdc.csv',index=False)
+    prevDeaths.to_csv('./csv/covid_deaths_cdc.csv',index=False)
+    prevTesting.to_csv('./csv/covid_testing_cdc.csv',index=False)
+    prevTestingPos.to_csv('./csv/covid_wk_pos_cdc.csv',index=False)
+    prevTestingCap.to_csv('./csv/covid_tcap_cdc.csv',index=False)
+    prevTestingCcpt.to_csv('./csv/covid_ccpt_cdc.csv',index=False)
 
 if __name__ == '__main__':
-    getCdcData()
+    downloadCDCdata()
+    parseCDCdata(glob('./xlsx/*.xlsx'))
