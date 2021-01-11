e3bb5b3a99b5e07893d9316c11e4dbff2935ed8d
Vidal Anguiano Jr <vidal.anguiano@gmail.com>
2021-01-11 16:31:18 -0600

    fix git add command

--- data-scripts/testing/run_testing.sh
+++ data-scripts/testing/run_testing.sh
@@ -18,7 +18,7 @@ cd /tmp/covid
 git add ./docs/csv/covid_ccpt_1p3a_state.csv \
         ./docs/csv/covid_tcap_1p3a_state.csv \
         ./docs/csv/covid_wk_pos_1p3a_state.csv \
-        ./docs/csv/covid_testing_1p3a_state.csv\
+        ./docs/csv/covid_testing_1p3a_state.csv
 # git add ./docs/csv/covid_ccpt_usafacts_state.csv \
 #         ./docs/csv/covid_tcap_usafacts_state.csv \
 #         ./docs/csv/covid_wk_pos_usafacts_state.csv \
