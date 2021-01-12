c1b5a062a66198bd75d76c6ef8edc0738ffc075b
steph-yang <38839004+steph-yang@users.noreply.github.com>
2021-01-12 03:42:18 -0600

    update hospital coordinate script and data

--- hospital/fetch_hospital_coordinate.py
+++ hospital/fetch_hospital_coordinate.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
-from geopy.geocoders import Nominatim
+from geopy.geocoders import ArcGIS 
 import json
 
 
@@ -11,19 +11,31 @@ def fetch_hospital_coordinate():
 	hospital = data.groupby(['hospital_pk','hospital_name', 'address']).size().reset_index().drop([0], axis=1).set_index('hospital_pk')
 
 	hospital = hospital.to_dict("index")
+	geolocator = ArcGIS()
 
 	i = 0
 	for k, v in hospital.items():
-		locator = Nominatim(user_agent="myGeocoder")
-		location = locator.geocode(v["address"], timeout=10)
-		if location:
-			v["lat"] = location.latitude
-			v["lon"] = location.longitude
-		else:
+		try:
+			address, (lat, lon) = geolocator.geocode(v["hospital_name"], timeout=10)
+			v["lat"] = lat
+			v["lon"] = lon
+		except:
 			v["lat"] = -999
 			v["lon"] = -999
 		i += 1
 		print(i)
+
+	# Use hospital address to find coordinates for empty points, if exist
+	unresolved_points = {k:v for k,v in hospital.items() if v["lon"] == -999}
+	if unresolved_points:
+		for k, v in unresolved_points.items():
+			try:
+				address, (lat, lon) = geolocator.geocode(v["address"], timeout=10)
+				hospital[k]["lat"] = lat
+				hospital[k]["lon"] = lon
+			except:
+				hospital[k]["lat"] = -999
+				hospital[k]["lon"] = -999
 
 	with open('hospital.json', 'w') as fp:
 		json.dump(hospital, fp)
