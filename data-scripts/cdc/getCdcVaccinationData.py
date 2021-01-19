a5a8145e39919b17c5a3a94b3b448f036f3f138c
Dylan <d_h@mit.edu>
2021-01-18 19:34:34 -0600

    File Cleanup

--- data-scripts/cdc/getCdcVaccinationData.py
+++ data-scripts/cdc/getCdcVaccinationData.py
@@ -12,61 +12,9 @@ def downloadCDCVaccinationData():
 
     return glob('./json/*.json')
 
-def parse1p3aVaccinationData(vaccinationDataList):
-    dateDf = pd.read_csv('../../docs/csv/covid_confirmed_1p3a_state.csv')
+def parseVaccinationData(vaccinationDataList):
     geoidTable = pd.read_csv('./statename_geoid.csv')
-    datesList = list(dateDf.columns[2:])
-    datesList.sort()
-    datesList = ['GEOID', 'NAME'] + datesList
-    placeholderDf = pd.DataFrame(datesList).set_index(0).T
-
-    for idx, file in enumerate(vaccinationDataList):
-        with open(file) as f:
-            data = json.load(f)
-            if (type(data)==dict):
-                data = data['vaccination_data']
-        currDate = data[0]['Date']
-        vaccinationDf = pd.DataFrame(data) \
-            .merge(geoidTable, left_on="Location", right_on="STUSPS", how="inner")[['GEOID','NAME','Doses_Distributed','Doses_Administered']]
-
-        if idx == 0:
-            vaccineAdministered = vaccinationDf[['GEOID','NAME','Doses_Administered']]
-            vaccineDistributed = vaccinationDf[['GEOID','NAME','Doses_Distributed']]
-            vaccineAdministered.columns = ['GEOID','NAME',currDate]
-            vaccineDistributed.columns = ['GEOID','NAME',currDate]
-        else:
-            dailyVaccineAdministered = vaccinationDf[['GEOID','NAME','Doses_Administered']]
-            dailyVaccineDistributed = vaccinationDf[['GEOID','NAME','Doses_Distributed']]
-            dailyVaccineAdministered.columns = ['GEOID','NAME',currDate]
-            dailyVaccineDistributed.columns = ['GEOID','NAME',currDate]
-
-            vaccineAdministered = vaccineAdministered.merge(dailyVaccineAdministered, on=["GEOID","NAME"])
-            vaccineDistributed = vaccineDistributed.merge(dailyVaccineDistributed, on=["GEOID","NAME"])
-
-    merged = pd.concat([placeholderDf, vaccineAdministered], sort=True)
-    cols = list(merged.columns)[-2:] + list(merged.columns)[:-2]
-    merged = merged[cols]
-    merged.to_csv()
-
-    merged.to_csv('./csv/vaccine_admin_cdc_1p3a_state.csv', index=False)
-    merged.to_csv('../../docs/csv/vaccine_admin_cdc_1p3a_state.csv', index=False)
-
-    merged = pd.concat([placeholderDf, vaccineDistributed], sort=True)
-    cols = list(merged.columns)[-2:] + list(merged.columns)[:-2]
-    merged = merged[cols]
-    merged.to_csv()
-
-    merged.to_csv('./csv/vaccine_dist_cdc_1p3a_state.csv', index=False)
-    merged.to_csv('../../docs/csv/vaccine_dist_cdc_1p3a_state.csv', index=False)
-
-def parseNytVaccinationData(vaccinationDataList):
-    dateDf = pd.read_csv('../../docs/csv/covid_confirmed_nyt_state.csv')
-    geoidTable = pd.read_csv('./statename_geoid.csv')
-    datesList = list(dateDf.columns[1:])
-    datesList.sort()
-    datesList = ['fips'] + datesList
-    placeholderDf = pd.DataFrame(datesList).set_index(0).T
-
+    
     for idx, file in enumerate(vaccinationDataList):
         with open(file) as f:
             data = json.load(f)
@@ -90,23 +38,13 @@ def parseNytVaccinationData(vaccinationDataList):
             vaccineAdministered = vaccineAdministered.merge(dailyVaccineAdministered, on=["fips"])
             vaccineDistributed = vaccineDistributed.merge(dailyVaccineDistributed, on=["fips"])
 
-    merged = pd.concat([placeholderDf, vaccineAdministered], sort=True)
-    cols = list(merged.columns)[-1:] + list(merged.columns)[:-1]
-    merged = merged[cols]
-    merged.to_csv()
-
-    merged.to_csv('./csv/vaccine_admin_cdc_nyt_state.csv', index=False)
-    merged.to_csv('../../docs/csv/vaccine_admin_cdc_nyt_state.csv', index=False)
-
-    merged = pd.concat([placeholderDf, vaccineDistributed], sort=True)
-    cols = list(merged.columns)[-1:] + list(merged.columns)[:-1]
-    merged = merged[cols]
-    merged.to_csv()
-
-    merged.to_csv('./csv/vaccine_dist_cdc_nyt_state.csv', index=False)
-    merged.to_csv('../../docs/csv/vaccine_dist_cdc_nyt_state.csv', index=False)
+    return { 'vaccineAdministered': vaccineAdministered, 'vaccineDistributed': vaccineDistributed }
 
 if __name__ == "__main__":
     fileList = downloadCDCVaccinationData()
-    parse1p3aVaccinationData(fileList)
-    parseNytVaccinationData(fileList)
+    parsedData = parseVaccinationData(fileList)
+
+    parsedData['vaccineDistributed'].to_csv('./csv/vaccine_dist_cdc.csv', index=False)
+    parsedData['vaccineDistributed'].to_csv('../../docs/csv/vaccine_dist_cdc.csv', index=False)
+    parsedData['vaccineAdministered'].to_csv('./csv/vaccine_admin_cdc.csv', index=False)
+    parsedData['vaccineAdministered'].to_csv('../../docs/csv/vaccine_admin_cdc.csv', index=False)
