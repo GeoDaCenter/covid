7cb0186617ffddc30a7256b5e8500b8a3b3f8db5
Dylan <d_h@mit.edu>
2021-02-25 14:02:02 -0600

    Disable Redux-Persist

--- src/index.js
+++ src/index.js
@@ -7,24 +7,24 @@ import WebFont from 'webfontloader';
 import registerServiceWorker from "./registerServiceWorker";
 import { createStore } from 'redux';
 import rootReducer from './reducers';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-import { PersistGate } from 'redux-persist/integration/react';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+// import { PersistGate } from 'redux-persist/integration/react';
+// import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  stateReconciler: autoMergeLevel2 ,
-  whitelist: [] // only navigation will be persisted 'dataParams', 'mapParams', 'currentData'
-}
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   stateReconciler: autoMergeLevel2 ,
+//   whitelist: [] // only navigation will be persisted 'dataParams', 'mapParams', 'currentData'
+// }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
 const store = createStore(
-  persistedReducer
+  rootReducer
   // ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-const persistor = persistStore(store)
+// const persistor = persistStore(store)
 
 
 WebFont.load({
