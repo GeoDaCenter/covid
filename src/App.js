4fb7b5b4c74a469c059b03858878240db920270c
theuscovidatlas <dhalpern@uchicago.edu>
2021-07-09 14:15:13 -0500

    Link Fix

--- src/App.js
+++ src/App.js
@@ -50,9 +50,9 @@ class App extends Component {
                 <Route path="/faq.html" component={Faq}/>
                 <Route path="/conduct" component={CodeOfConduct}/>
                 <Route path="/conduct.html" component={CodeOfConduct}/>
-                <Route exact path="/500000" render={() => {window.location.href="./500000/index.html"}} />
-                <Route exact path="/500000/index.html" render={() => {window.location.href="./500000/index.html"}} />
-                <Route exact path="/500000.html" render={() => {window.location.href="./500000/index.html"}} />
+                <Route exact path="/500000" render={() => {window.location.href="500000/index.html"}} />
+                <Route exact path="/500000/index.html" render={() => {window.location.href="500000/index.html"}} />
+                <Route exact path="/500000.html" render={() => {window.location.href="500000/index.html"}} />
                 <Route component={ErrorPage} />
                 <Route />
             </Switch>
