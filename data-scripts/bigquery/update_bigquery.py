0e9329ed1e797cbe061c91080640c3b0ea2c0c25
theuscovidatlas <dhalpern@uchicago.edu>
2021-08-18 13:02:53 -0500

    [BigQuery] Handle Different ID Columns [skip ci]

--- data-scripts/bigquery/update_bigquery.py
+++ data-scripts/bigquery/update_bigquery.py
@@ -28,7 +28,7 @@ repo_root = os.path.abspath(os.path.join(dir_path, '..', '..')) # /path-to-repo/
 print('dir_path: ', dir_path)
 print('repo_root: ', repo_root)
 
-def write_table_to_bq(table, project_id, dataset_id, columns_dic):
+def write_table_to_bq(table, project_id, dataset_id, columns_dic, id_col=False):
     '''
     Write table to bigquery and append columns to a dictionary
     '''
@@ -36,7 +36,9 @@ def write_table_to_bq(table, project_id, dataset_id, columns_dic):
     input_file = os.path.join(repo_root, 'public/csv', table)
     print('writing: ', input_file)
     df = pd.read_csv(input_file)
-    
+    if id_col != False:
+        df.columns = ['fips_code' if col == id_col else col for col in list(df.columns)]
+
     for col in df.columns:
         new_col = re.sub(r'\-',r' ', col)
         new_col = re.sub(r'\s+',r'_', new_col)
@@ -105,7 +107,58 @@ safegraph_lst = ['mobility_fulltime_workdays_safegraph.csv',
                  'mobility_home_workdays_safegraph.csv',
                  'mobility_parttime_workdays_safegraph.csv']
 
-
+id_col_dict = {
+    'berkeley_predictions.csv':'fips',
+    'chr_health_context.csv':'FIPS', 
+    'chr_health_context_state.csv':'FIPS',
+    'chr_health_factors.csv':'FIPS',
+    'chr_health_factors_state.csv':'FIPS',
+    'chr_life.csv':'FIPS',
+    'chr_life_state.csv':'FIPS',
+    'context_essential_workers_acs.csv':'fips',
+    'context_fqhc_clinics_hrsa.csv':False,
+    'context_hospitals_covidcaremap.csv':False,
+    'context_vaccination_sites_hrsa_wh.csv':False,
+    'covid_confirmed_cdc.csv':'fips_code', 
+    'covid_confirmed_cdc_state.csv':'fips_code',
+    'covid_confirmed_nyt.csv':'fips', 
+    'covid_confirmed_nyt_state.csv':'fips',
+    'covid_confirmed_usafacts.csv':'countyFIPS', 
+    'covid_confirmed_usafacts_h.csv':'countyFIPS', 
+    'covid_confirmed_usafacts_state.csv':'stateFIPS',
+    'covid_deaths_cdc.csv':'fips_code', 
+    'covid_deaths_cdc_state.csv':'fips_code',
+    'covid_deaths_nyt.csv':'fips', 
+    'covid_deaths_nyt_state.csv':'fips',
+    'covid_deaths_usafacts.csv':'countyFIPS', 
+    'covid_deaths_usafacts_h.csv':'countyFIPS', 
+    'covid_deaths_usafacts_state.csv':'stateFIPS',
+    'covid_ccpt_cdc.csv':'state_fips',
+    'covid_ccpt_cdc_state.csv':'fips_code', 
+    'covid_tcap_cdc.csv':'fips_code', 
+    'covid_tcap_cdc_state.csv':'state_fips',
+    'covid_testing_cdc.csv':'fips_code', 
+    'covid_testing_cdc_state.csv':'state_fips',
+    'covid_wk_pos_cdc.csv':'fips_code', 
+    'covid_wk_pos_cdc_state.csv':'state_fips',
+    'vaccination_fully_vaccinated_cdc.csv':'fips',
+    'vaccination_fully_vaccinated_cdc_h.csv':'fips',
+    'vaccination_fully_vaccinated_cdc_state.csv':'fips',
+    'vaccination_one_or_more_doses_cdc.csv':'fips', 
+    'vaccination_one_or_more_doses_cdc_h.csv':'fips', 
+    'vaccination_one_or_more_doses_cdc_state.csv':'fips', 
+    'vaccination_to_be_distributed_cdc.csv':'fips',
+    'vaccination_to_be_distributed_cdc_state.csv':'fips',
+    'vaccine_fully_vaccinated_cdc.csv':'fips',
+    'vaccine_fully_vaccinated_cdc_h.csv':'fips',
+    'covid_confirmed_1p3a.csv':'GEOID',
+    'covid_confirmed_1p3a_state.csv':'GEOID',
+    'covid_deaths_1p3a.csv':'GEOID', 
+    'covid_deaths_1p3a_state.csv':'GEOID',
+    'mobility_fulltime_workdays_safegraph.csv':'county', 
+    'mobility_home_workdays_safegraph.csv':'county', 
+    'mobility_parttime_workdays_safegraph.csv':'county'
+}
 
 if __name__ == "__main__":
 
@@ -123,15 +176,15 @@ if __name__ == "__main__":
 
     for table in public_lst:
         dataset_id = 'public'
-        write_table_to_bq(table, project_id, dataset_id, columns_dic)
+        write_table_to_bq(table, project_id, dataset_id, columns_dic, id_col_dict[table])
 
     for table in _1P3A_lst:
         dataset_id = '1P3A'
-        write_table_to_bq(table, project_id, dataset_id, columns_dic)
+        write_table_to_bq(table, project_id, dataset_id, columns_dic, id_col_dict[table])
 
     for table in safegraph_lst:
         dataset_id = 'safegraph'
-        write_table_to_bq(table, project_id, dataset_id, columns_dic)
+        write_table_to_bq(table, project_id, dataset_id, columns_dic, id_col_dict[table])
 
     with open(os.path.join(repo_root,'functions/meta/columns.json'), 'w') as write_file:
         json.dump(columns_dic, write_file, indent=4)
