706d5fca77b81220d33be2c7e6cbeba9ed7bea61
Dylan <dhalpern@uchicago.edu>
2022-01-12 19:24:29 -0600

    Pbf Sharding (node)

--- build-scripts/buildTimeseries.js
+++ build-scripts/buildTimeseries.js
@@ -1,104 +1,121 @@
-const fs = require('fs');
-const Pbf = require('pbf');
-const path = require('path');
+const fs = require("fs");
+const Pbf = require("pbf");
+const path = require("path");
+const Rows = require("./pbfReadSchema").Rows;
+const Schema = require("./pbfWriteSchema");
 
-// Entry ========================================
-
-var Entry = {};
-
-Entry.read = function (pbf, end) {
-  return pbf.readFields(Entry._readField, { geoid: 0, vals: [] }, end);
-};
-Entry._readField = function (tag, obj, pbf) {
-  if (tag === 1) obj.geoid = pbf.readVarint(true);
-  else if (tag === 2) pbf.readPackedVarint(obj.vals, true);
-};
-Entry.write = function (obj, pbf) {
-  if (obj.geoid) pbf.writeVarintField(1, obj.geoid);
-  if (obj.vals) pbf.writePackedVarint(2, obj.vals);
-};
-
-// Rows ========================================
-
-var Rows = {};
-
-Rows.read = function (pbf, end) {
-  return pbf.readFields(Rows._readField, { dates: [], row: [] }, end);
-};
-Rows._readField = function (tag, obj, pbf) {
-  if (tag === 1) obj.dates.push(pbf.readString());
-  else if (tag === 2) obj.row.push(Entry.read(pbf, pbf.readVarint() + pbf.pos));
-};
-Rows.write = function (obj, pbf) {
-  if (obj.dates)
-    for (var i = 0; i < obj.dates.length; i++)
-      pbf.writeStringField(1, obj.dates[i]);
-  if (obj.row)
-    for (i = 0; i < obj.row.length; i++)
-      pbf.writeMessage(2, Entry.write, obj.row[i]);
-};
-
-
-const directoryPath = path.join(__dirname, '../public/pbf/');
+const directoryPath = path.join(__dirname, "../public/pbf/");
 
 const cleanVals = (vals, multiplier) => {
-    const returnVals = [];
-    for (let i=0; i<vals.length; i++) {
-        if (vals[i] === -999 || vals[i] === -9999) {
-            returnVals.push(null);
-        } else {
-            returnVals.push(vals[i] * multiplier);
-        }
+  const returnVals = [];
+  for (let i = 0; i < vals.length; i++) {
+    if (vals[i] === -999 || vals[i] === -9999) {
+      returnVals.push(null);
+    } else {
+      returnVals.push(vals[i] * multiplier);
     }
-    return returnVals;
-}
+  }
+  return returnVals;
+};
 //passsing directoryPath and callback function
-console.log('Building timeseries...');
-const t0 = new Date()
+console.log("Building timeseries...");
+const makeFolder = (fileName) => {
+  try {
+    fs.mkdirSync(`public/timeseries`);
+  } catch {}
+
+  try {
+    fs.mkdirSync(`public/timeseries/${fileName}`);
+  } catch {}
+};
+
+const generateIndividualFiles = (fileName, multiplier, row) => {
+  let sumData = [];
+  for (let i = 0; i < row.length; i++) {
+    const entry = row[i];
+    const values = cleanVals(entry.vals, multiplier);
+    fs.writeFileSync(
+      `public/timeseries/${fileName}/${entry.geoid}.json`,
+      `${JSON.stringify(values)}`
+    );
+    for (let j = 0; j < values.length; j++) {
+      sumData[j] = (values[j] || 0) + (sumData[j] || 0);
+    }
+  }
+  return sumData;
+};
+
+const generateSummary = (fileName, dates, sumData, multiplier, row) => {
+  fs.writeFileSync(
+    `public/timeseries/${fileName}/index.json`,
+    `{
+        "dates": ${JSON.stringify(dates)},
+        "sumData": ${JSON.stringify(sumData)},
+        "multiplier": ${multiplier},
+        "count": ${row.length},
+        "ids": ${JSON.stringify(row.map((entry) => entry.geoid))}
+    }`
+  );
+};
+const onlyUnique = (value, index, self) => self.indexOf(value) === index;
+const t0 = new Date();
 fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        if (!(file.includes('2020') || file.includes('2021') || file.includes('latest') || file.includes('dotDensity'))){
-            const fileName = file.split('.')[0]
-            const multiplier = file.split('.').length > 2
-                ? +file.split('.')[1].split('-')[1]
-                : 1
-            const data = fs.readFileSync(`public/pbf/${file}`)
-            const pbf = new Pbf(data);
-            const {dates,row} = Rows.read(pbf);
-            try {
-                fs.mkdirSync(`public/timeseries`);
-            } catch {}
+  //handling error
+  if (err) {
+    return console.log("Unable to scan directory: " + err);
+  }
+  files.forEach(function (file) {
+    // exclude dot density and already-parsed files.
+    if (
+      !(
+        file.includes("2020") ||
+        file.includes("2021") ||
+        file.includes("latest") ||
+        file.includes("dotDensity")
+      )
+    ) {
+      const fileName = file.split(".")[0];
+      makeFolder(fileName);
+      const multiplier =
+        file.split(".").length > 2 ? +file.split(".")[1].split("-")[1] : 1;
+      const { dates, row } = Rows.read(
+        new Pbf(fs.readFileSync(`public/pbf/${file}`))
+      );
+      const reversedDates = [...dates].reverse();
+      const sumData = generateIndividualFiles(fileName, multiplier, row);
+      generateSummary(fileName, dates, sumData, multiplier, row);
+      const months = dates.map((date) => date.slice(0, -3)).filter(onlyUnique);
+      const indexRanges = months.reduce(
+        (prev, curr) => [
+          ...prev,
+          [
+            dates.findIndex((date) => date.includes(curr)),
+            dates.length -
+              reversedDates.findIndex((date) => date.includes(curr)),
+          ],
+        ],
+        []
+      );
+      // generate rows
+      for (let i=0; i<months.length;i++){
+          const month = i === months.lenght - 1
+            ? 'latest'
+            : months[i];
+          const range = indexRanges[i];
+          const monthData = new Schema.Rows();
+          const rowData = row.map(data => {
+              const entry = new Schema.Entry();
+              entry.setGeoid(data.geoid);
+              entry.setValsList(data.vals.slice(range[0], range[1]));
+              return entry;
+          });
+          monthData.setDatesList(dates.slice(range[0], range[1]));
+          monthData.setRowList(rowData);
+          const binaryData = monthData.serializeBinary()
+          fs.writeFileSync(`public/pbf/${fileName}.${month}.pbf`, binaryData);
+      }
+    }
+  });
 
-            try {
-                fs.mkdirSync(`public/timeseries/${fileName}`);
-            } catch {}
-
-            // if (fileName === 'covid_confirmed_nyt') console.log(row)
-            let sumData = [];
-            for (let i=0; i<row.length; i++) {
-              const entry = row[i];
-              const values = cleanVals(entry.vals, multiplier);
-              fs.writeFileSync(`public/timeseries/${fileName}/${entry.geoid}.json`,  `${JSON.stringify(values)}`)
-              for (let j=0; j<values.length; j++) {
-                sumData[j] = (values[j]||0) + (sumData[j]||0);
-              }
-            }
-            
-            // if (fileName === 'covid_confirmed_nyt') console.log(sumData)
-            fs.writeFileSync(`public/timeseries/${fileName}/index.json`,  `{
-                "dates": ${JSON.stringify(dates)},
-                "sumData": ${JSON.stringify(sumData)},
-                "multiplier": ${multiplier},
-                "count": ${row.length},
-                "ids": ${JSON.stringify(row.map(entry => entry.geoid))}
-            }`)
-        }
-    });
-
-    console.log(`Done in ${new Date() - t0} ms`);
+  console.log(`Done in ${new Date() - t0} ms`);
 });
---
+++ build-scripts/pbfReadSchema.js
@@ -0,0 +1,39 @@
+
+// Entry ========================================
+
+var Entry = {};
+Entry.read = function (pbf, end) {
+  return pbf.readFields(Entry._readField, { geoid: 0, vals: [] }, end);
+};
+Entry._readField = function (tag, obj, pbf) {
+  if (tag === 1) obj.geoid = pbf.readVarint(true);
+  else if (tag === 2) pbf.readPackedVarint(obj.vals, true);
+};
+Entry.write = function (obj, pbf) {
+  if (obj.geoid) pbf.writeVarintField(1, obj.geoid);
+  if (obj.vals) pbf.writePackedVarint(2, obj.vals);
+};
+
+// Rows ========================================
+
+var Rows = {};
+
+Rows.read = function (pbf, end) {
+  return pbf.readFields(Rows._readField, { dates: [], row: [] }, end);
+};
+Rows._readField = function (tag, obj, pbf) {
+  if (tag === 1) obj.dates.push(pbf.readString());
+  else if (tag === 2) obj.row.push(Entry.read(pbf, pbf.readVarint() + pbf.pos));
+};
+Rows.write = function (obj, pbf) {
+  if (obj.dates)
+    for (var i = 0; i < obj.dates.length; i++)
+      pbf.writeStringField(1, obj.dates[i]);
+  if (obj.row)
+    for (i = 0; i < obj.row.length; i++)
+      pbf.writeMessage(2, Entry.write, obj.row[i]);
+};
+
+module.exports = {
+    Entry, Rows
+}
