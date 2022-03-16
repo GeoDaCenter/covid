bfa171411c49b5baaf91b9b14208ce039d47b96c
spaykin <spaykin@uchicago.edu>
2022-03-16 14:56:50 -0500

    Update research brief text

--- src/components/Pages/Insights.js
+++ src/components/Pages/Insights.js
@@ -132,7 +132,6 @@ const research = [
             <br/>
             <b>Meaning</b>  Findings from this study suggest the need for future research that addresses health inequity and guides policies and programs by further exploring the different dimensions and regional patterns of social determinants of health.
             <br /><br/>
-            <a href="https://uscovidatlas.org/research-briefs/structural-barriers-covid.pdf">Read the Research Brief</a>
         </p>,
         'thumbnail': `${process.env.PUBLIC_URL}/research/structuralBarriers.jpg`,
         'alt': 'Four maps highlighting the number of days each county spent in the most severe 20% of COVID-19 mortality.',
@@ -286,7 +285,7 @@ export default function Insights({
                                     {!!entry.researchBrief && <>
                                     <Gutter h={10} />
                                     <a href={entry.researchBrief} target="_blank" rel="noopener noreferrer">
-                                        Research the research brief
+                                        Read the research brief
                                     </a></>}
                                 </Grid>
                             </ProductCard>
