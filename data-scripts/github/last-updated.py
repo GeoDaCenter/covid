abc5580bcf325d6b5bc1912028597bff2e50c6c5
Dylan <dhalpern@uchicago.edu>
2022-11-04 12:07:33 -0500

    Fix for timestamp and add to data pull

--- data-scripts/github/last-updated.py
+++ data-scripts/github/last-updated.py
@@ -1,9 +1,9 @@
 import os
-from datetime import time
+from datetime import datetime
 
 if __name__ == "__main__":
     filename = os.getenv("FILENAME")
-    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # WRITE A FILE TO THE CURRENT DIRECTORY with the current time
     with open(f"{filename}.txt", "w") as f:
         f.write(current_time)
